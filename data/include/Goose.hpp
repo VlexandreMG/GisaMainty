62d5e9f08c3fc1d530aedd182b81000167c8afcb
VlexandreMG <dodolashmou@gmail.com>
2026-09-07 00:26:21 +0300

    Done function chekcCollision

--- data/include/Goose.hpp
+++ data/include/Goose.hpp
@@ -4,6 +4,8 @@ class Goose {
 public:
     float x;
     float y;
+    float w = 15.0f;
+    float h = 15.0f;
     int attackFrameLeft = 0;
     int health = 3;
 
@@ -11,6 +13,25 @@ public:
         if (attackFrameLeft <= 0) {
             attackFrameLeft = 15;
         }
+    }
+
+    bool checkCollision(const Goose& other) const {
+        float MonMurDroit = x;
+        float MonMurGauche = x+w;
+        float MonMurHaut = y;
+        float MonMurBas = y+h;
+
+        float OtMurDroit = other.x;
+        float OtMurGauche = other.x + other.w;
+        float OtMurHaut = other.y;
+        float OtMurBas = other.y + other.h;
+
+        bool CollGauche = MonMurDroit > OtMurGauche;
+        bool CollDroite = MonMurGauche < OtMurDroit;
+        bool CollHaut = MonMurHaut < OtMurBas;
+        bool CollBas = MonMurBas > OtMurHaut;
+
+        return CollDroite && CollGauche && CollHaut && CollBas; 
     }
 
     void update() {
