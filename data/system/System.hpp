956e95206c4da0f7154fe514a420e6d1bdb7d7ec
VlexandreMG <dodolashmou@gmail.com>
2026-09-08 05:16:55 +0300

    Add function applyPhysics

--- data/system/System.hpp
+++ data/system/System.hpp
@@ -3,5 +3,18 @@
 
 class System {
 public:
-    
+    float GRAVITY = 0.3f;
+    float GROUNDED_Y = 200.0f;
+
+    void applyPhysics(Goose& goose) {
+        goose.vy += GRAVITY;
+
+        goose.y += goose.vy;
+
+        if ((goose.y + goose.h) > GROUNDED_Y) {
+            goose.y = GROUNDED_Y - goose.h;
+            goose.vy = 0;
+            goose.isGrounded = true;
+        } 
+    }
 }
