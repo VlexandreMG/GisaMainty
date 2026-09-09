b14d3483e5979145be3a728ee6e8a1350dd7f5db
VlexandreMG <dodolashmou@gmail.com>
2026-09-09 22:56:06 +0300

    Add 3 functions on GooseManager

--- data/manager/GooseManager.hpp
+++ data/manager/GooseManager.hpp
@@ -1,7 +1,30 @@
 #pragma once 
+#include <vector>
 #include "../include/Goose.hpp"
 
 class GooseManager {
 public:
-    
+    std::vector<Goose*> geese;
+
+    Goose* spawnGoose(float X, float Y) {
+       Goose* newGoose = new Goose();
+       newGoose->x = X;
+       newGoose->y = Y;
+
+       geese.push_back(newGoose);
+       return newGoose;
+    }
+
+    void updateAll() {
+        for (Goose* goose : geese) {
+            goose.update();
+        }
+    }
+
+    void clear() {
+        for (Goose* goose : geese) {
+            delete goose;
+        }
+        geese.clear();
+    }
 }
